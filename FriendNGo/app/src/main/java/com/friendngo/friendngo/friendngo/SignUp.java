b43d7d659a7fe5954a2af73aa6d57b976828ea14
Krishna <parth1493@gmail.com>
2017-03-03 11:42:17 -0500

    Merge commit

--- FriendNGo/app/src/main/java/com/friendngo/friendngo/friendngo/SignUp.java
+++ FriendNGo/app/src/main/java/com/friendngo/friendngo/friendngo/SignUp.java
@@ -28,6 +28,7 @@ public class SignUp extends AppCompatActivity {
     private TextView textView;
     private Button signupButton;
 
+    
     private EditText emailEditTextValue;
     private EditText passwordEditTextValue;
 
