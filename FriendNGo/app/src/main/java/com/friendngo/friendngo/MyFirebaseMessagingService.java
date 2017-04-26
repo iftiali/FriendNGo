ead35d9499127199351f27f1e3805b9af7e7e76b
Krishna <parth1493@gmail.com>
2017-04-26 18:10:20 -0400

    Notification with UI.

--- FriendNGo/app/src/main/java/com/friendngo/friendngo/MyFirebaseMessagingService.java
+++ FriendNGo/app/src/main/java/com/friendngo/friendngo/MyFirebaseMessagingService.java
@@ -1,5 +1,10 @@
 package com.friendngo.friendngo;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -20,6 +25,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             // message, here is where that should be initiated.
             Log.d(TAG, "From: " + remoteMessage.getFrom());
             Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+            // Create Notification
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 1410,
+                    intent, PendingIntent.FLAG_ONE_SHOT);
+
+            NotificationCompat.Builder notificationBuilder = new
+                    NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.icon_map)
+                    .setContentTitle("Message")
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager =
+                    (NotificationManager)
+                            getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(1410, notificationBuilder.build());
         }
 
 
